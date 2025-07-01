30bc5808b954015fe5ff7c0ef4bd4e54c19c2c43
Michał Wiśniewski Front End Developer with backend skills <michalwisniewskifrontend@onet.pl>
2025-07-01 19:37:31 +0200

    Create Orders.js

---
+++ models/Orders.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose')
+
+const ordersSchema = new mongoose.Schema({
+    name: {type: String, required: true},
+    surname: {type: String, required: true},
+    street: {type: String, required: true},
+    postcode: {type: String, required: true},
+    city: {type: String, required: true},
+    companyname: {type: String, required: true},
+    companystreet: {type: String, required: true},
+    companypostcode: {type: String, required: true},
+    companycity: {type: String, required: true},
+    email: {type: String, required: true},
+    invoice: {type: Boolean, required: true},
+    login: {type: String, required: true},
+    newsletter: {type: Boolean, required: true},
+    password: {type: String, required: true},
+    phonenumber: {type: String, required: true},
+    regulations: {type: Boolean, required: true},
+    companynip: {type: Number, required: true},
+    companyregon: {type: Number, required: true},
+    ordercontent: {type: String, required: true},
+    orderamount: {type: Number, required: true},
+    ordertime: {type: String, required: true},
+
+    
+})
+
+const Orders = mongoose.model("Orders", ordersSchema)
+
+module.exports =  Orders
