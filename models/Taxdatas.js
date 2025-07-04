1ca7c2f39bcb2db69e254d643e9f2b27a714de69
Michał Wiśniewski Front End Developer with backend skills <michalwisniewskifrontend@onet.pl>
2025-07-04 18:22:13 +0200

    Create Taxdatas.js

---
+++ models/Taxdatas.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose')
+
+const taxdatasSchema = new mongoose.Schema({
+    sellercompanyname: {type: String, required: true},
+    sellercompanystreet: {type: String, required: true},
+    sellercompanypostcode: {type: String, required: true},
+    sellercompanycity: {type: String, required: true},
+    sellercompanynip: {type: String, required: true},
+    sellercompanyregon: {type: String, required: true},
+    invoicesactualnumber: {type: Number, required: true},
+    vatpercentage: {type: Number, required: true},
+    basisforvatexemption: {type: String, required: true},
+    
+})
+
+const Taxdatas = mongoose.model("Taxdatas", taxdatasSchema)
+
+module.exports = Taxdatas
