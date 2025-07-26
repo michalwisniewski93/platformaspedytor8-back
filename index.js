6e764190e478b959b47efc2307e017caafbe6c0b
Michał Wiśniewski Front End Developer with backend skills <michalwisniewskifrontend@onet.pl>
2025-07-26 17:15:10 +0200

    Update index.js

--- index.js
+++ index.js
@@ -89,7 +89,7 @@ const resetInvoicesNumber = async () => {
 
 
 // MongoDB połączenie
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true, serverSelectionTimeoutMS: 10000, })
   .then(() => {
     console.log("Connected to MongoDB");
     resetInvoicesNumber(); // <<< WYWOŁANIE FUNKCJI PO POŁĄCZENIU
