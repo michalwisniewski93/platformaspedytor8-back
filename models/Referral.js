e9c7b9e6f38b90457aa9de72a0efda67deee995f
Michał Wiśniewski Front End Developer with backend skills <michalwisniewskifrontend@onet.pl>
2025-09-13 12:58:27 +0200

    Update Referral.js

--- models/Referral.js
+++ models/Referral.js
@@ -1,1 +1,10 @@
+const mongoose = require('mongoose')
 
+const referralSchema = new mongoose.Schema({
+  source: { type: String, required: true },
+  count: { type: Number, default: 1 },
+});
+
+const Referral = mongoose.model("Referral", referralSchema);
+
+export default Referral;
