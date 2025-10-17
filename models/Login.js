57e2f5efb3235fe0ab9d90d77b147d978c029009
AKCode-eu <kontakt@akcode.eu>
2025-10-17 08:16:00 +0200

    Update Login.js

--- models/Login.js
+++ models/Login.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose')
 
 const loginSchema = new mongoose.Schema({
-    login: {type: String, required: true, select: false},
-    password: { type: String, required: true, select: false },
+    login: {type: String, required: true},
+    password: { type: String, required: true},
 })
 
 const Login = mongoose.model("Login", loginSchema)
