2eb68e37b2ed03306f1a85d0507a812cd9a08870
alannadolnyy <alannadolnyy@gmail.com>
2025-10-21 23:36:50 +0200

    Move allowed origins to separate file

---
+++ consts/allowedOrigins.js
@@ -0,0 +1,8 @@
+const ALLOWED_ORIGINS = [
+    'https://platformaspedytor24-front.vercel.app',
+    'https://spedytorszkolenia.pl',
+    'https://www.spedytorszkolenia.pl',
+    'http://localhost:3000',
+];
+
+module.exports = ALLOWED_ORIGINS;
