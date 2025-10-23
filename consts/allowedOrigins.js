ae462f3b29f36782e3415adcf9a9d2b49ddc35ce
alannadolnyy <alannadolnyy@gmail.com>
2025-10-23 13:29:57 +0200

    Replace import with require

--- consts/allowedOrigins.js
+++ consts/allowedOrigins.js
@@ -2,6 +2,7 @@ const ALLOWED_ORIGINS = [
     'https://platformaspedytor24-front.vercel.app',
     'https://spedytorszkolenia.pl',
     'https://www.spedytorszkolenia.pl',
+    'http://localhost:3000',
 ];
 
 module.exports = ALLOWED_ORIGINS;
