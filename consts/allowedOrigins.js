a4b5cd6cd2f4258dd918859fec756b437494b767
alannadolnyy <alannadolnyy@gmail.com>
2025-10-23 13:31:20 +0200

    Remove localhost

--- consts/allowedOrigins.js
+++ consts/allowedOrigins.js
@@ -2,7 +2,6 @@ const ALLOWED_ORIGINS = [
     'https://platformaspedytor24-front.vercel.app',
     'https://spedytorszkolenia.pl',
     'https://www.spedytorszkolenia.pl',
-    'http://localhost:3000',
 ];
 
 module.exports = ALLOWED_ORIGINS;
